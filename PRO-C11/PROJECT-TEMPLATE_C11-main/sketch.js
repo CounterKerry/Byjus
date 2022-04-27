8aed6089dc2a92ba4eff8d0304925058c7086301
CounterKerry <ribsilguel@gmail.com>
2022-04-26 22:38:27 -0300

    Criando arquivos da aula PRO-C11

---
+++ PRO-C11/PROJECT-TEMPLATE_C11-main/sketch.js
@@ -0,0 +1,27 @@
+var ship, shipAnimation;
+var sea, seaImage;
+
+function preload(){
+shipAnimation = loadAnimation("ship-1.png", "ship-2.png", "ship-3.png", "ship-4.png");
+seaImage = loadImage("sea.png");
+}
+
+function setup(){
+  createCanvas(400,400);
+  
+}
+
+function draw() {
+  background("blue");
+    drawSprites();
+
+    
+    sea = createSprite(200, 220, 200, 30);
+    sea.addImage(seaImage)
+    sea.scale = 0.30
+    ship = createSprite(190, 200, 80, 30);
+    ship.addAnimation("animation", shipAnimation);
+    ship.scale = 0.25;
+
+ 
+}
